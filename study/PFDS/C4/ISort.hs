566d52432458976c8def151f111d3c813ced689b
rf <rf0444@gmail.com>
2012-01-21 18:02:39 +0900

    isort' を削除

--- study/PFDS/C4/ISort.hs
+++ study/PFDS/C4/ISort.hs
@@ -3,16 +3,18 @@ module PFDS.C4.ISort where
 
 insert :: Ord a => a -> [a] -> [a]
 insert x [] = [x]
-insert x ys@(y:ys') = f $ compare x y -- ← y をとってくる時に後ろの isort' が評価される
+insert x ys@(y:ys') = f $ compare x y -- ← y をとってくる時に後ろの isort が評価される
  where
   f GT = y : insert x ys' -- ← ここの insert が遅延
   f _ = x : ys
 
 isort :: Ord a => [a] -> [a]
-isort xs = isort' [] xs
+--isort xs = isort' [] xs
+isort [] = []
+isort (x:xs) = insert x (isort xs)
 
-isort' :: Ord a => [a] -> [a] -> [a]
-isort' ys [] = ys
---isort' ys (x:xs) = isort' (insert x ys) xs
-isort' ys (x:xs) = insert x (isort' ys xs)
+--isort' :: Ord a => [a] -> [a] -> [a]
+--isort' ys [] = ys
+----isort' ys (x:xs) = isort' (insert x ys) xs
+--isort' ys (x:xs) = insert x (isort' ys xs)
 
