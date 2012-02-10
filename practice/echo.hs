3315712433ce35ae9b4751745b64e25d7e162fcb
rf <rf0444@gmail.com>
2012-02-10 12:49:55 +0900

    副作用のあるプログラムを書いてみる

---
+++ practice/echo.hs
@@ -0,0 +1,31 @@
+import Control.Applicative
+
+main :: IO ()
+main = proc (/= "exit") >> putStrLn "exit program."
+ where
+  proc :: (String -> Bool) -> IO [String]
+  proc f = takeWhileM f procs
+   where
+    procs :: [IO String]
+    procs = (=<<) (printIf f) <$> prompt "> " <$> inputs
+
+takeWhileM :: Monad m => (a -> Bool) -> [m a] -> m [a]
+takeWhileM _ [] = return []
+takeWhileM f (x:xs) = do
+  ele <- x
+  if f ele then do
+    xs' <- takeWhileM f xs
+    return (ele : xs')
+  else return []
+
+inputs :: [IO String]
+inputs = getLine : inputs
+
+prompt :: String -> IO String -> IO String
+prompt = (>>) . putStr
+
+printIf :: (String -> Bool) -> String -> IO String
+printIf f ele
+ | f ele = putStrLn ele >> return ele
+ | otherwise = return ele
+
