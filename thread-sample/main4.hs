7288a1bf01f02b99b258c0593932eebc56f359e3
rf <rf0444@gmail.com>
2013-02-11 19:23:02 +0900

    IO の後ろに隠した
    1つ目のIOをはがすと開始、
    2つ目のIOをはがすと終了待ち。

--- thread-sample/main4.hs
+++ thread-sample/main4.hs
@@ -1,23 +1,22 @@
 import Control.Concurrent
-import Control.Exception (SomeException, mask, throw, try)
-import Control.Monad (join)
+import Control.Exception (SomeException, mask, throwIO, try)
 
 main :: IO ()
 main = do
     putStrLn "begin"
-    mvar <- fork $ do
+    future <- fork $ do
         putStrLn "hello"
         error "error!"
         putStrLn "world"
-    putStrLn "waitin..."
-    join $ takeMVar mvar
+    putStrLn "waiting..."
+    _ <- future
     putStrLn "end"
 
-fork :: IO a -> IO (MVar (IO a))
+fork :: IO a -> IO (IO a)
 fork proc = do
     mvar <- newEmptyMVar
-    forkFinally proc $ putMVar mvar . either throw return
-    return mvar
+    forkFinally proc $ putMVar mvar
+    return $ takeMVar mvar >>= either throwIO return
 
 -- copy from base-4.6
 forkFinally :: IO a -> (Either SomeException a -> IO ()) -> IO ThreadId
